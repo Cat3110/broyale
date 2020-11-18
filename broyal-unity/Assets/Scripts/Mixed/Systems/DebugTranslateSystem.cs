37c6c3e9bd7c103eaec20a99f2338221c5588ae6
Livithan <livithan@gmail.com>
2020-11-19 03:48:14 +0700

    Debug system

---
+++ broyal-unity/Assets/Scripts/Mixed/Systems/DebugTranslateSystem.cs
@@ -0,0 +1,33 @@
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.NetCode;
+using UnityEngine;
+
+namespace None
+{
+    [DisableAutoCreation]
+    [UpdateInGroup(typeof(ClientAndServerSimulationSystemGroup), OrderLast = true)]
+    public class DebugTranslateSystem : SystemBase
+    {
+        private bool _isServer;
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            _isServer = World.GetExistingSystem<ServerSimulationSystemGroup>() != null;
+        }
+
+        protected override void OnUpdate()
+        {
+            var predictionGroup = World.GetExistingSystem<GhostPredictionSystemGroup>();
+            var tick = predictionGroup.PredictingTick;
+            var deltaTime = Time.DeltaTime;
+            var last = predictionGroup.IsFinalPredictionTick;
+
+            Entities.ForEach((ref Entity e, ref Translation position, ref MovableCharacterComponent mv) =>
+            {
+                Grapher.Log((Vector2)position.Value.xz,$"{ (_isServer ? "Server" : "Client") } {e.Index}", (float)Time.ElapsedTime);
+            }).WithoutBurst().Run();
+        }
+    }
+}
